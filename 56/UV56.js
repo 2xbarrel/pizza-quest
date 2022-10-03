1b40f185dcbe12a71d99f38c1217f77a1cdb641a
2xbarrel <barrelthegreat@gmail.com>
2022-10-03 21:17:02 +0300

    plz work ;(

--- 56/UV56.js
+++ 56/UV56.js
@@ -1,5 +1,5 @@
 function update(e){
-    let walter = document.getElementById("walter");
+    let walter = document.getElementById("walter").getBoundingClientRect();
     let x = e.clientX || e.touches[0].clientX
     let y = e.clientY || e.touches[0].clientY
   
--- 62/62.js
+++ 62/62.js
@@ -7,24 +7,25 @@ function wait(milliseconds){
 
 const spook = document.getElementById("spook");
 
-// let checked = false;
-// spook.addEventListener("pointerenter", event => {
-//   if (!checked) {
-//     new Audio("bam.mp3").play();
-//   }
-// });
+let checked = false;
+spook.addEventListener("pointerenter", async (event) => {
+  if (!checked) {
+    new Audio("bam.mp3").play();
+    await wait(2000);
+    window.open("../41/pizaquest41.html", "_self")
+  }
+});
 
-// spook.addEventListener("pointerleave", event => {
-//   checked = true;
+spook.addEventListener("pointerleave", event => {
+  checked = true;
+});
+
+
+// spook.addEventListener("pointerenter", async (event) => {
+//   new Audio("bam.mp3").play();
+// })
+
+// spook.addEventListener("pointerenter", async (event) => {
+//   await wait(2000);
 //   window.open("../41/pizaquest41.html", "_self")
-// });
-
-
-spook.addEventListener("mouseenter", async (event) => {
-  new Audio("bam.mp3").play();
-})
-
-spook.addEventListener("mouseenter", async (event) => {
-  await wait(2000);
-  window.open("../41/pizaquest41.html", "_self")
-})
+// })
