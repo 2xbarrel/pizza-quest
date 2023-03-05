c9c6fb108337ebc8ca05cbb4cdc5dcebc54ef64a
2xbarrel <barrelthegreat@gmail.com>
2023-03-05 23:09:54 +0300

    terminal test

---
+++ funny/tests/terminal/terminal.js
@@ -0,0 +1,123 @@
+function changeText(){
+    let input = document.getElementById("input").value;
+    if (input == "correct password"){
+        document.getElementById("output").innerHTML = "correct password yippee!";
+    }
+    else if (input == "lu"){
+        document.getElementById("output").innerHTML = "hi lu!!!1!!";
+    }
+    else if (input == "pom"){
+        document.getElementById("output").innerHTML = "hi pom!!!1!!";
+    }
+    else if (input == "azzman"){
+        document.getElementById("output").innerHTML = "azzman more like like ass like assman";
+    }
+    else if (input == "juno"){
+        document.getElementById("output").innerHTML = "juno, more like, june, like the month, gottem";
+    }
+    else if (input == "taroo"){
+        document.getElementById("output").innerHTML = "v-word";
+    }
+    else if (input == "hi"){
+        document.getElementById("output").innerHTML = "ayyy sup girl :lip-bite:";
+    }
+    else if (input == "sachiko"){
+        document.getElementById("output").innerHTML = "wrong comic dipshit";
+    }
+    else if (input == "fibbit"){
+        document.getElementById("output").innerHTML = "keep drawing fam you doing good";
+    }
+    else if (input == "harmonizerr"){
+        document.getElementById("output").innerHTML = "ALEX THE LION??? WHAT THE HELL ARE YOU DOING IN MY CAR";
+    }
+    else if (input == "seeker"){
+        document.getElementById("output").innerHTML = "he is our savious and he is our light, all shall repent in his holy sight";
+    }
+    else if (input == "guard"){
+        document.getElementById("output").innerHTML = "terminal log number 23: subject has breached contaiment but doesnt show any signs of hostility";
+    }
+    else if (input == "janitor"){
+        document.getElementById("output").innerHTML = "terminal log number 56: after our janitor mr.Robenson fell into the tank full of experimental material his flesh and isides became parts of his hazmat suit, he doesnt show any signs of life.";
+    }
+    else if (input == "berry"){
+        document.getElementById("output").innerHTML = "yummy";
+    }
+    else if (input == "pizza quest"){
+        document.getElementById("output").innerHTML = ";)";
+    }
+    else if (input == "false host"){
+        document.getElementById("output").innerHTML = ":0";
+    }
+    else if (input == "deez nuts"){
+        document.getElementById("output").innerHTML = "AHAHAHAHAHHAHAHAHAHAHAHAHAHAHA GOD YOU ARE SO FUCKING FUNNY MAN I AM DYING OF LAUGHTER kill yourself";
+    }
+    else if (input == "how are you doing"){
+        document.getElementById("output").innerHTML = "eh fine whateva";
+    }
+    else if (input == "javascript"){
+        document.getElementById("output").innerHTML = "let myBalls = document.getElementByID('BIG')";
+    }
+    else if (input == "kitsunivy"){
+        document.getElementById("output").innerHTML = "блять как рамку у кнопки убрать";
+    }
+    else if (input == "$m"){
+        document.getElementById("output").innerHTML = "wrong channel dipshit";
+    }
+    else if (input == "zof"){
+        document.getElementById("output").innerHTML = "hi zof!!!1!!";
+    }
+    else if (input == "blyat"){
+        document.getElementById("output").innerHTML = "i am going to fucking kill you";
+    }
+    else if (input == "блять"){
+        document.getElementById("output").innerHTML = "shut up";
+    }
+    else if (input == "блять"){
+        document.getElementById("output").innerHTML = "shut up";
+    }
+    else if (input == "jef"){
+        document.getElementById("output").innerHTML = "you and I are not so different";
+    }
+    else if (input == "hack"){
+        document.getElementById("output").innerHTML = "oh no im hacked";
+    }
+    else if (input == "password"){
+        document.getElementById("output").innerHTML = "couldnt you write anything more original?";
+    }
+    else if (input == "burrito"){
+        document.getElementById("output").innerHTML = "burrito quest is real";
+    }
+    else if (input == ""){
+        document.getElementById("output").innerHTML = "you didnt write anything dipshit";
+    }
+    else if (input == "are you ok"){
+        document.getElementById("output").innerHTML = "no";
+    }
+    else if (input == "4x4"){
+        document.getElementById("output").innerHTML = "NO YOU CANT SAY THAT";
+    }
+    else if (input == "test"){
+        document.getElementById("output").innerHTML = "yep its working";
+    }
+    else if (input == "barrel"){
+        document.getElementById("output").innerHTML = "as sad as it is, this password is wrong";
+    }
+    else if (input == "tell me a joke"){
+        document.getElementById("output").innerHTML = "no fuck off";
+    }
+    else if (input == "what is a correct password"){
+        document.getElementById("output").innerHTML = "its 'correct password'";
+    }
+    else if (input == "yippee"){
+        document.getElementById("output").innerHTML = "waaaaaaaaaaaaaa";
+    }
+    else if (input == "takahiro"){
+        document.getElementById("output").innerHTML = "wrong comic goddamnit";
+    }
+    else if (input == "god"){
+        document.getElementById("output").innerHTML = "seeker";
+    }
+    else {
+        document.getElementById("output").innerHTML = "WRONG YOU DUMB SHIT ABSOLUTE BAFOON GOD HOW FUCKING DUMB ARE YOU JESUS CHRIST";
+    };
+};
