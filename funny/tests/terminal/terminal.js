4f18aa924cdba2cf615379bacf6680df44e2f05a
2xbarrel <barrelthegreat@gmail.com>
2023-10-03 21:42:54 +0300

    password better

--- funny/tests/terminal/terminal.js
+++ funny/tests/terminal/terminal.js
@@ -1,147 +1,99 @@
-function changeText(){
-    let input = document.getElementById("input").value;
-    if (input == "correct password"){
-        document.getElementById("output").innerHTML = "correct password yippee!";
-    }
-    else if (input == "lu"){
-        document.getElementById("output").innerHTML = "hi lu!!!1!!";
-    }
-    else if (input == "pom"){
-        document.getElementById("output").innerHTML = "hi pom!!!1!!";
-    }
-    else if (input == "azzman"){
-        document.getElementById("output").innerHTML = "azzman more like like ass like assman";
-    }
-    else if (input == "juno"){
-        document.getElementById("output").innerHTML = "juno, more like, june, like the month, gottem";
-    }
-    else if (input == "taroo"){
-        document.getElementById("output").innerHTML = "v-word";
-    }
-    else if (input == "hi"){
-        document.getElementById("output").innerHTML = "ayyy sup girl :lip-bite:";
-    }
-    else if (input == "sachiko"){
-        document.getElementById("output").innerHTML = "wrong comic dipshit";
-    }
-    else if (input == "fibbit"){
-        document.getElementById("output").innerHTML = "keep drawing fam you doing good";
-    }
-    else if (input == "harmonizerr"){
-        document.getElementById("output").innerHTML = "ALEX THE LION??? WHAT THE HELL ARE YOU DOING IN MY CAR";
-    }
-    else if (input == "seeker"){
-        document.getElementById("output").innerHTML = "he is our savious and he is our light, all shall repent in his holy sight";
-    }
-    else if (input == "guard"){
-        document.getElementById("output").innerHTML = "terminal log number 23: subject has breached contaiment but doesnt show any signs of hostility";
-    }
-    else if (input == "janitor"){
-        document.getElementById("output").innerHTML = "terminal log number 56: after our janitor mr.Robenson fell into the tank full of experimental material his flesh and isides became parts of his hazmat suit, he doesnt show any signs of life.";
-    }
-    else if (input == "berry"){
-        document.getElementById("output").innerHTML = "yummy";
-    }
-    else if (input == "pizza quest"){
-        document.getElementById("output").innerHTML = ";)";
-    }
-    else if (input == "false host"){
-        document.getElementById("output").innerHTML = ":0";
-    }
-    else if (input == "deez nuts"){
-        document.getElementById("output").innerHTML = "AHAHAHAHAHHAHAHAHAHAHAHAHAHAHA GOD YOU ARE SO FUCKING FUNNY MAN I AM DYING OF LAUGHTER kill yourself";
-    }
-    else if (input == "how are you doing"){
-        document.getElementById("output").innerHTML = "eh fine whateva";
-    }
-    else if (input == "javascript"){
-        document.getElementById("output").innerHTML = "let myBalls = document.getElementByID('BIG')";
-    }
-    else if (input == "kitsunivy"){
-        document.getElementById("output").innerHTML = "блять как рамку у кнопки убрать";
-    }
-    else if (input == "$m"){
-        document.getElementById("output").innerHTML = "wrong channel dipshit";
-    }
-    else if (input == "zof"){
-        document.getElementById("output").innerHTML = "hi zof!!!1!!";
-    }
-    else if (input == "blyat"){
-        document.getElementById("output").innerHTML = "i am going to fucking kill you";
-    }
-    else if (input == "блять"){
-        document.getElementById("output").innerHTML = "shut up";
-    }
-    else if (input == "jef"){
-        document.getElementById("output").innerHTML = "you and I are not so different";
-    }
-    else if (input == "hack"){
-        document.getElementById("output").innerHTML = "oh no im hacked";
-    }
-    else if (input == "password"){
-        document.getElementById("output").innerHTML = "couldnt you write anything more original?";
-    }
-    else if (input == "burrito"){
-        document.getElementById("output").innerHTML = "burrito quest is real";
-    }
-    else if (input == ""){
-        document.getElementById("output").innerHTML = "you didnt write anything dipshit";
-    }
-    else if (input == "are you ok"){
-        document.getElementById("output").innerHTML = "no";
-    }
-    else if (input == "4x4"){
-        document.getElementById("output").innerHTML = "NO YOU CANT SAY THAT";
-    }
-    else if (input == "test"){
-        document.getElementById("output").innerHTML = "yep its working";
-    }
-    else if (input == "barrel"){
-        document.getElementById("output").innerHTML = "as sad as it is, this password is wrong";
-    }
-    else if (input == "tell me a joke"){
-        document.getElementById("output").innerHTML = "no fuck off";
-    }
-    else if (input == "what is the correct password"){
-        document.getElementById("output").innerHTML = "its 'correct password'";
-    }
-    else if (input == "yippee"){
-        document.getElementById("output").innerHTML = "waaaaaaaaaaaaaa";
-    }
-    else if (input == "takahiro"){
-        document.getElementById("output").innerHTML = "wrong comic goddamnit";
-    }
-    else if (input == "god"){
-        document.getElementById("output").innerHTML = "seeker";
-    }
-    else if (input == "juno is gay"){
-        document.getElementById("output").innerHTML = "he is??? damnnn :lipbite: whats his @";
-    }
-    else if (input == "junoisgay"){
-        document.getElementById("output").innerHTML = "he is??? damnnn :lipbite: whats his @";
-    }
-    else if (input == "junoisbiggay"){
-        document.getElementById("output").innerHTML = "he is??? damnnn :lipbite: whats his @";
-    }
-    else if (input == "junolikesdick"){
-        document.getElementById("output").innerHTML = "he is??? damnnn :lipbite: whats his @";
-    }
-    else if (input == "junoisahomo"){
-        document.getElementById("output").innerHTML = "he is??? damnnn :lipbite: whats his @";
-    }
-    else if (input == "junoisafemboy"){
-        document.getElementById("output").innerHTML = "he is??? damnnn :lipbite: whats his @";
-    }
-    else if (input == "barrel sucks"){
-        document.getElementById("output").innerHTML = "NO I DONT, YOU DO, SHUT UP";
-    }
-    else if (input == "cock"){
-        document.getElementById("output").innerHTML = "balls";
-    }
-    else if (input == "balls"){
-        document.getElementById("output").innerHTML = "cock";
+const pswd = [
+    "8d2ac8b58ead9744d77286de9b0bcb7a894f238c3149fc9f3b1e3caff36330fe", // 1
+    "712985cc1194dffe8b30127444e0c92fe214420a84a5b2d28abe6684f4540409", // 2 
+];
+
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+
+function sha256(ascii) {  // I DIDNT WRITE THIS FUNCTION, IT GOT IT FROM HERE!!!!! -> https://geraintluff.github.io/sha256/
+	function rightRotate(value, amount) {
+		return (value>>>amount) | (value<<(32 - amount));
+	};
+	
+	let mathPow = Math.pow;
+	let maxWord = mathPow(2, 32);
+	let lengthProperty = 'length'
+	let i, j; 
+	let result = ''
+
+	let words = [];
+	let asciiBitLength = ascii[lengthProperty]*8;
+	
+	let hash = sha256.h = sha256.h || [];
+	let k = sha256.k = sha256.k || [];
+	let primeCounter = k[lengthProperty];
+
+	let isComposite = {};
+	for (let candidate = 2; primeCounter < 64; candidate++) {
+		if (!isComposite[candidate]) {
+			for (i = 0; i < 313; i += candidate) {
+				isComposite[i] = candidate;
+			}
+			hash[primeCounter] = (mathPow(candidate, .5)*maxWord)|0;
+			k[primeCounter++] = (mathPow(candidate, 1/3)*maxWord)|0;
+		}
+	}
+	
+	ascii += '\x80' 
+	while (ascii[lengthProperty]%64 - 56) ascii += '\x00' 
+	for (i = 0; i < ascii[lengthProperty]; i++) {
+		j = ascii.charCodeAt(i);
+		if (j>>8) return; 
+		words[i>>2] |= j << ((3 - i)%4)*8;
+	}
+	words[words[lengthProperty]] = ((asciiBitLength/maxWord)|0);
+	words[words[lengthProperty]] = (asciiBitLength)
+	
+	for (j = 0; j < words[lengthProperty];) {
+		let w = words.slice(j, j += 16); 
+		let oldHash = hash;
+		hash = hash.slice(0, 8);
+		
+		for (i = 0; i < 64; i++) {
+			let i2 = i + j;
+			let w15 = w[i - 15], w2 = w[i - 2];
+
+			let a = hash[0], e = hash[4];
+			let temp1 = hash[7]
+				+ (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) 
+				+ ((e&hash[5])^((~e)&hash[6])) 
+				+ k[i]
+				+ (w[i] = (i < 16) ? w[i] : (
+						w[i - 16]
+						+ (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15>>>3)) 
+						+ w[i - 7]
+						+ (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2>>>10)) 
+					)|0
+				);
+			let temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) 
+				+ ((a&hash[1])^(a&hash[2])^(hash[1]&hash[2])); 
+			
+			hash = [(temp1 + temp2)|0].concat(hash); 
+			hash[4] = (hash[4] + temp1)|0;
+		}
+		
+		for (i = 0; i < 8; i++) {
+			hash[i] = (hash[i] + oldHash[i])|0;
+		}
+	}
+	
+	for (i = 0; i < 8; i++) {
+		for (j = 3; j + 1; j--) {
+			let b = (hash[i]>>(j*8))&255;
+			result += ((b < 16) ? 0 : '') + b.toString(16);
+		}
+	}
+	return result;
+};
+
+
+function inscryption() {
+    let truepswd = sha256(input.value);
+    if (pswd.includes(truepswd)){
+        output.innerHTML = "correct passworde :) youroe win th gaem";
     }
     else {
-        document.getElementById("output").innerHTML = "WRONG YOU DUMB SHIT ABSOLUTE BAFOON GOD HOW FUCKING DUMB ARE YOU JESUS CHRIST";
-    };
+        output.innerHTML = "wrong, fak u";
+    }
 };
