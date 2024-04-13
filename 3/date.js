3cca391de9e35da409491c2eb0b60ee1b4d4ae9d
2xbarrel <barrelthegreat@gmail.com>
2024-04-13 23:16:43 +0300

    iso and quazi

--- 3/date.js
+++ 3/date.js
@@ -1,5 +1,7 @@
 let img = document.getElementById("img");
 let title = document.getElementById("title");
+let text = document.getElementById("text");
+
 
 let date = new Date();
 
@@ -7,4 +9,16 @@ if (date.getDate() === 13 && date.getMonth() === 3){
   img.src = "homestuck.png";
   title.innerHTML = "PIZZASTUCK";
   document.body.style.backgroundColor = "white";
+};
+
+if (date.getDate() === 21 && date.getMonth() === 4){
+  img.src = "quaziandiso.png";
+  title.innerHTML = "quazi quest";
+  text.innerHTML = "This is you-.... This is not you actually, this is Quazi and Iso, what the hell are they doing here?";
+};
+
+if (date.getDate() === 28 && date.getMonth() === 1){
+  img.src = "quaziandiso.png";
+  title.innerHTML = "iso quest";
+  text.innerHTML = "This is you-.... This is not you actually, this is Quazi and Iso, what the hell are they doing here?";
 };
