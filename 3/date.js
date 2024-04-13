c657d6e3ef76666550bafca893b55434556ac8a5
2xbarrel <barrelthegreat@gmail.com>
2024-04-13 13:05:11 +0300

    HOOOOMESTUCKED

--- 3/date.js
+++ 3/date.js
@@ -1,8 +1,10 @@
 let img = document.getElementById("img");
+let title = document.getElementById("title")
 
 let date = new Date();
 
 if (date.getDate() === 13 && date.getMonth() === 3){
   img.src = "homestuck.png";
+  title.innerHTML = "PIZZASTUCK"
   document.body.style.backgroundColor = "white";
 };
