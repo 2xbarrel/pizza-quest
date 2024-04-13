5e027de2f871014da4e893fc26967a8450eaa39c
2xbarrel <barrelthegreat@gmail.com>
2024-04-13 13:02:48 +0300

    homestucked

---
+++ 3/date.js
@@ -0,0 +1,8 @@
+let img = document.getElementById("img");
+
+let date = new Date();
+
+if (date.getDate() === 13 && date.getMonth() === 3){
+  img.src = "homestuck.png";
+  document.body.style.backgroundColor = "white";
+};
